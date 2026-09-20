cbdacab7e27f2352d4fad0187bfbb9a371c07fa7
guidomercado-gamer <guido.mercado@ug.uchile.cl>
2026-09-20 08:41:44 +0200

    Add motor pin configuration for L298N driver

--- Codigo/Control_Motores_Avance1.ino
+++ Codigo/Control_Motores_Avance1.ino
@@ -1,1 +1,10 @@
+// ─── CONFIGURACIÓN DE PINES (ARDUINO UNO -> DRIVER L298N) ───
+// Motor Izquierdo
+const int IN1 = 7;   // Sentido de giro 1 (Pin digital)
+const int IN2 = 8;   // Sentido de giro 2 (Pin digital)
+const int ENA = 5;   // Control de velocidad PWM (Pin con ~)
 
+// Motor Derecho
+const int IN3 = 4;   // Sentido de giro 1 (Pin digital)
+const int IN4 = 3;   // Sentido de giro 2 (Pin digital)
+const int ENB = 9;   // Control de velocidad PWM (Pin con ~)
